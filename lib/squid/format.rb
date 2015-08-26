2951689c76a1b33080dc3a7901792a3e0576fc60
claudiob <claudiob@gmail.com>
2015-08-26 10:42:11 -0700

    Change format: :float to round to 2 digits
    Otherwise the weirdness of Ruby Float might cause labels to look
    like 6.00000000001

--- lib/squid/format.rb
+++ lib/squid/format.rb
@@ -10,7 +10,7 @@ module Squid
         when :percentage then number_to_percentage value, precision: 1
         when :currency then number_to_currency value
         when :seconds then number_to_minutes_and_seconds value
-        when :float then number_to_delimited value
+        when :float then number_to_float value
         else number_to_delimited value.to_i
       end.to_s
     end
@@ -18,6 +18,11 @@ module Squid
     def number_to_minutes_and_seconds(value)
       signum = '-' if value < 0
       "#{signum}#{value.abs.round/60}:#{(value.abs.round%60).to_s.rjust 2, '0'}"
+    end
+
+    def number_to_float(value)
+      float = number_to_rounded value, significant: true, precision: 2
+      number_to_delimited float
     end
   end
 end
